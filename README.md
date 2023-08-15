99c21c1f2c46d84634816f59f64c7da489c84570
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2023-08-15 19:38:14 +1000

    get started

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # nci-scripts
-PBS scripts for running nanopore genomics analyses on NCI Gadi
 
+PBS scripts for running nanopore genomics analyses on Australia's [NCI Gadi Supercomputer](https://nci.org.au/our-systems/hpc-systems).
 
-Join if89 project in NCI. 
+These scripts uses software installed under the if89 project. If you have an NCI account, simply join the if89 project using the [myNCI](https://my.nci.org.au/mancini/login?next=/mancini/) and start using these scripts with software already installed on Gadi through project if89. See the comments in the scripts to identify which variables you should change.
+
+## S/BLOW5 Basecalling
+
+- [basecall/buttery-eel.pbs.sh](basecall/buttery-eel.pbs.sh) - Basecall a S/BLOW5 file using [buttery-eel](https://github.com/Psy-Fer/buttery-eel) wrapper for Guppy
+- [basecall/slow5-dorado.pbs.sh](basecall/slow5-dorado.pbs.sh) - Basecall a S/BLOW5 file using [slow5-dorado](https://github.com/hiruna72/slow5-dorado/releases/), a fork of ONT's Dorado that supports S/BLOW5.
--- basecall/buttery-eel.pbs.sh
+++ basecall/buttery-eel.pbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -P wv19
-#PBS -N SLOW5-DORADO
+#PBS -N EEL
 #PBS -q gpuvolta
 #PBS -l ncpus=48
 #PBS -l ngpus=4
@@ -11,32 +11,36 @@
 
 ###################################################################
 
-MODEL=/g/data/if89/apps/slow5-dorado/0.2.1/slow5-dorado/models/dna_r10.4.1_e8.2_400bps_sup@v4.1.0
+ONT_GUPPY_PATH=/g/data/if89/apps/buttery-eel/0.3.1/ont-guppy-6.4.2/bin
+MODEL=dna_r10.4.1_e8.2_400bps_sup_prom.cfg
 
 ###################################################################
 
 # Make sure to change:
 # 1. wv19 to your own project
-# 2. the name of the Model
+# 2. the name of the Guppy model
 
 # to run:
-# qsub -v MERGED_SLOW5=/path/to/reads.blow5,BASECALL_OUT=/path/to/out/dir ./slow5-dorado.pbs.sh
+# qsub -v MERGED_SLOW5=/path/to/reads.blow5,BASECALL_OUT=/path/to/out/dir ./buttery-eel.pbs.sh
 
 ###################################################################
 
 usage() {
-	echo "Usage: qsub -v MERGED_SLOW5=/g/data/wv19/public/hg2_prom_lsk114_subsubsample/reads.blow5,BASECALL_OUT=/scratch/wv19/hg1112/tmp/hg2_prom_lsk114_subsubsample/ ./slow5-dorado.pbs.sh" >&2
+	echo "Usage: qsub -v MERGED_SLOW5=/g/data/wv19/public/hg2_prom_lsk114_subsubsample/reads.blow5,BASECALL_OUT=/scratch/wv19/hg1112/tmp/hg2_prom_lsk114_subsubsample/ ./buttery-eel.pbs.sh" >&2
 	echo
 	exit 1
 }
 
-# directory where basecalls should be written to
+#directory where basecalls should be written to
 [ -z "${BASECALL_OUT}" ] && usage
-#where the merged BLOW5 file is
+# merged BLOW5
 [ -z "${MERGED_SLOW5}" ] && usage
 
-module load /g/data/if89/apps/modulefiles/slow5-dorado/0.2.1
-num_threads=${PBS_NCPUS}
+module load /g/data/if89/apps/modulefiles/buttery-eel/0.3.1
+
+###################################################################
+
+PORT=5000
 
 # terminate script
 die() {
@@ -52,6 +56,6 @@ test -e ${MERGED_SLOW5} || die "${MERGED_SLOW5} not found. Exiting."
 mkdir ${BASECALL_OUT} || die "Creating directory ${BASECALL_OUT} failed. Exiting."
 cd ${BASECALL_OUT} || die "${MERGED_SLOW5} not found. Exiting."
 
-/usr/bin/time -v  slow5-dorado basecaller ${MODEL} ${MERGED_SLOW5} --emit-fastq > ${BASECALL_OUT}/reads.fastq  -x cuda:all
+/usr/bin/time -v  buttery-eel -i ${MERGED_SLOW5} -o ${BASECALL_OUT}/reads.fastq --guppy_bin ${ONT_GUPPY_PATH} --port ${PORT} --config ${MODEL} -x cuda:all --guppy_batchsize 20000 --max_queued_reads 20000 --slow5_threads 10 --slow5_batchsize 100 --procs 20 || die "basecalling failed"
 
 echo "basecalling success"
