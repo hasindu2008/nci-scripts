6b74ac65e492c50ccc3280277ff171b8e9cb97fa
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2024-08-27 09:48:28 +1000

    update model name

--- modcall/buttery-eel-dorado.pbs.sh
+++ modcall/buttery-eel-dorado.pbs.sh
@@ -12,11 +12,7 @@
 ###################################################################
 
 # Change this to the model you want to use
-MODEL=dna_r10.4.1_e8.2_400bps_5khz_modbases_5mc_cg_sup_prom.cfg
-# MODEL=dna_r10.4.1_e8.2_400bps_modbases_5mc_cg_sup_prom.cfg
-# MODEL=dna_r10.4.1_e8.2_400bps_modbases_5mc_cg_hac_prom.cfg
-# MODEL=dna_r9.4.1_450bps_modbases_5mc_cg_sup_prom.cfg
-# MODEL=dna_r9.4.1_450bps_modbases_5mc_cg_hac_prom.cfg
+MODEL=dna_r10.4.1_e8.2_400bps_5khz_modbases_5hmc_5mc_cg_sup_prom.cfg
 
 ###################################################################
 
